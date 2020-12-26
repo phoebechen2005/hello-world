c9142bda90beab23f84e5c2bbdcc87d9c700b43d
phoebechen2005 <74422531+phoebechen2005@users.noreply.github.com>
2020-12-26 13:58:37 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row} from 'react-bootstrap';
+import RFANavbar from "./components/RFANavbar";
+// import RFACard from "./components/RFACard";
+import RFAAffOrgs from "./components/About/RFAAffOrgs";
+export default class App extends React.Component{
+
+  render() {
+    return (
+      <Container fluid>
+        <Row> {/* Structure of webpage will follow grid layout => row, column, container */}
+          <Container>
+              <header>
+                  <RFANavbar/>
+              </header>
+          </Container>
+        </Row>
+        <RFAAffOrgs/>
+
+        {/* <Container>
+          <Row>
+            {
+              ["one", "two", "three"].map(element => (
+                <Col>
+                  Image
+                  Text
+                </Col>
+              ))
+            }
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <CardDeck>
+              <RFACard />
+              <RFACard />
+              <RFACard />
+              <RFACard />
+            </CardDeck>
+          </Row>
+        </Container> */}
+      </Container>
+    );
+  }
+}
